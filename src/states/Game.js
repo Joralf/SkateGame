bb2cbb6e9ac2caae94c1631c55693ffc87b10940
Joralf Quist <joralfquist@gmail.com>
2018-01-25 16:53:30 +0100

    last commit

--- src/states/Game.js
+++ src/states/Game.js
@@ -123,8 +123,8 @@ export default class extends Phaser.State {
     }
   }
   render () {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.player, 32, 32)
-    }
+    // if (__DEV__) {
+    //   this.game.debug.spriteInfo(this.player, 32, 32)
+    // }
   }
 }
